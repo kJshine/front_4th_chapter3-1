03133ece39692e8908ddaa87682e91f27a99ae0f
김재환 <fodore12@gmail.com>
2025-02-04 20:53:59 +0900

    feat: useSearch 테스트 코드 작성

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -1,14 +1,80 @@
 import { act, renderHook } from '@testing-library/react';
 
+import { FILTERED_EVENTS } from '../../__mocks__/response/mockEvents.ts';
 import { useSearch } from '../../hooks/useSearch.ts';
 import { Event } from '../../types.ts';
 
-it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {});
+describe('검색어 동작', () => {
+  it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {
+    const { result } = renderHook(() =>
+      useSearch(FILTERED_EVENTS, new Date('2024-07-01'), 'month')
+    );
 
-it('검색어에 맞는 이벤트만 필터링해야 한다', () => {});
+    act(() => result.current.setSearchTerm(''));
 
-it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {});
+    expect(result.current.filteredEvents).toEqual([
+      FILTERED_EVENTS[0],
+      FILTERED_EVENTS[1],
+      FILTERED_EVENTS[2],
+    ]);
+  });
 
-it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {});
+  it('검색어에 맞는 이벤트만 필터링해야 한다', () => {
+    const { result } = renderHook(() =>
+      useSearch(FILTERED_EVENTS, new Date('2024-07-01'), 'month')
+    );
 
-it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {});
+    act(() => result.current.setSearchTerm('이벤트 1'));
+
+    expect(result.current.filteredEvents).toEqual([FILTERED_EVENTS[0]]);
+  });
+
+  it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {
+    const { result } = renderHook(() =>
+      useSearch(FILTERED_EVENTS, new Date('2024-07-01'), 'month')
+    );
+
+    act(() => result.current.setSearchTerm('이벤트 1'));
+    expect(result.current.filteredEvents).toEqual([FILTERED_EVENTS[0]]);
+
+    act(() => result.current.setSearchTerm('기존 팀 미팅2'));
+    expect(result.current.filteredEvents).toEqual([FILTERED_EVENTS[1]]);
+
+    act(() => result.current.setSearchTerm('회의실 C'));
+    expect(result.current.filteredEvents).toEqual([FILTERED_EVENTS[2]]);
+  });
+
+  it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {
+    // 주간 뷰
+    const { result: weekResult } = renderHook(() =>
+      useSearch(FILTERED_EVENTS, new Date('2024-07-01'), 'week')
+    );
+    expect(weekResult.current.filteredEvents).toEqual([FILTERED_EVENTS[0]]);
+
+    // 월간 뷰
+    const { result: monthResult } = renderHook(() =>
+      useSearch(FILTERED_EVENTS, new Date('2024-07-01'), 'month')
+    );
+    expect(monthResult.current.filteredEvents).toEqual([
+      FILTERED_EVENTS[0],
+      FILTERED_EVENTS[1],
+      FILTERED_EVENTS[2],
+    ]);
+  });
+
+  it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
+    const { result } = renderHook(() =>
+      useSearch(FILTERED_EVENTS, new Date('2024-07-01'), 'month')
+    );
+
+    act(() => result.current.setSearchTerm('회의'));
+    expect(result.current.filteredEvents).toEqual([
+      FILTERED_EVENTS[0],
+      FILTERED_EVENTS[1],
+      FILTERED_EVENTS[2],
+    ]);
+
+    act(() => result.current.setSearchTerm('점심'));
+    expect(result.current.filteredEvents).toEqual([]);
+  });
+});
